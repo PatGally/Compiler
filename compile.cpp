fcf5a470012c1284e18fe7b904f236dd1c4e35ff
PatGally <149409313+PatGally@users.noreply.github.com>
2025-04-22 07:04:11 -0700

    changes made

--- compile.cpp
+++ compile.cpp
@@ -211,8 +211,8 @@ public:
 	~StrPostFixExpr() {
 		exprs.clear();
 	}
-	string eval() {
-		string result = "";
+	string* eval() {
+		string* result = *"";
 		vector<string> tempStack;
 
 		for (const string& token : exprs) {
@@ -221,6 +221,17 @@ public:
 				tempStack.pop_back();
 				string a = tempStack.back();
 				tempStack.pop_back();
+
+				if (vartable.find(a) != vartable.end() && vartable.find(b) != vartable.end()) {
+                    a = vartable[a];
+					b = vartable[b];
+                }
+				else if (vartable.find(a) != vartable.end()) {
+                    a = vartable[a];
+                }
+                else if (vartable.find(b) != vartable.end()) {
+                    b = vartable[b];
+                }
 
 				if (token == "==") {
 					tempStack.push_back(a == b ? "" : nullptr);
@@ -240,7 +251,8 @@ public:
                     tempStack.push_back(a >= b ? "" : nullptr);
                 }
                 else if (token == "and") {
-                	tempStack.push_back((!a.empty() && !b.empty()) ? "" : nullptr);}
+                	tempStack.push_back((!a.empty() && !b.empty()) ? "" : nullptr);
+                }
                 else if (token == "or") {
                     tempStack.push_back(!a.empty() || !b.empty() ? "" : nullptr);
                 }
@@ -250,13 +262,14 @@ public:
 				else {
 				tempStack.push_back(token);
 				}
-			} else {
+			}
+			else {
 				tempStack.push_back(token);
 			}
 		}
 
 		if (!tempStack.empty()) {
-			result = tempStack.back();
+			result = &tempStack.back();
 		}
 		return result;
 	}
@@ -309,14 +322,13 @@ public:
 			vartable[var] = dynamic_cast<IntIdExpr*>(p_expr)->getId();
 		}
 		else if (dynamic_cast<StrIdExpr*>(p_expr)) {
-			vartable[var] = vartable[dynamic_cast<StrIdExpr*>(p_expr)->getId()];
+			vartable[var] = dynamic_cast<StrIdExpr*>(p_expr)->getId();
 		}
 		else if (dynamic_cast<IntPostFixExpr*>(p_expr)) {
 			vartable[var] = dynamic_cast<IntPostFixExpr*>(p_expr)->eval();
 		}
 		else if (dynamic_cast<StrPostFixExpr*>(p_expr)) {
-			vartable[var] = dynamic_cast<StrPostFixExpr*>(p_expr)->eval();
-		}
+			vartable[var] = *(dynamic_cast<StrPostFixExpr*>(p_expr)->eval());		}
 		else {
 			cerr << "Error: Invalid expression type" << endl;
 			exit(-1);
@@ -408,7 +420,7 @@ public:
 			cout << dynamic_cast<StrPostFixExpr*>(p_expr)->eval() << endl;
 		}
 		else if (dynamic_cast<StrIdExpr*>(p_expr)) {
-			cout << vartable[dynamic_cast<StrIdExpr*>(p_expr)->getId()] << endl;
+			cout << dynamic_cast<StrIdExpr*>(p_expr)->getId() << endl;
 		}
 		else if (dynamic_cast<IntIdExpr*>(p_expr)) {
 			cout << dynamic_cast<IntIdExpr*>(p_expr)->getId() << endl;
@@ -468,8 +480,8 @@ public:
               ++pc;
        }
     } else if (strPostFixExpr) {
-      string val = strPostFixExpr->eval();
-      if (val == "") {
+      string* val = strPostFixExpr->eval();
+      if (*val == "") {
         pc = elsetarget;
       } else {
         ++pc;
@@ -493,7 +505,7 @@ public:
     }
 };
 
-class WhileStmt : public Stmt{
+class WhileStmt : public Stmt {
 private:
 	Expr* p_expr;
 	int elsetarget;
@@ -502,14 +514,14 @@ public:
 		p_expr = nullptr;
 		elsetarget = 0;
 	}
-    WhileStmt(Expr* expr, int target){
-      p_expr = expr;
-      elsetarget = target;
+	WhileStmt(Expr* expr, int target){
+		p_expr = expr;
+		elsetarget = target;
 
-    }
+	}
 	~WhileStmt(){
-        if (p_expr == nullptr)
-		delete p_expr;
+		if (p_expr == nullptr)
+			delete p_expr;
 	}
 
 	void setTarget(int target) {
@@ -519,324 +531,325 @@ public:
 		return "While " + p_expr->toString() + " goto " + to_string(elsetarget);
 	}
 	void execute() {	//Check for the other 4 expressions
-	    IntegerConstExpr* intExpr = dynamic_cast<IntegerConstExpr*>(p_expr);
-        StringConstExpr* strExpr = dynamic_cast<StringConstExpr*>(p_expr);
-        IntIdExpr* intIdExpr = dynamic_cast<IntIdExpr*>(p_expr);
-        StrIdExpr* strIdExpr = dynamic_cast<StrIdExpr*>(p_expr);
+		IntegerConstExpr* intExpr = dynamic_cast<IntegerConstExpr*>(p_expr);
+		StringConstExpr* strExpr = dynamic_cast<StringConstExpr*>(p_expr);
+		IntIdExpr* intIdExpr = dynamic_cast<IntIdExpr*>(p_expr);
+		StrIdExpr* strIdExpr = dynamic_cast<StrIdExpr*>(p_expr);
 		StrPostFixExpr* strPostFixExpr = dynamic_cast<StrPostFixExpr*>(p_expr);
 		IntPostFixExpr* intPostFixExpr = dynamic_cast<IntPostFixExpr*>(p_expr);
-	    if (intExpr) {
-	        int val = intExpr->eval();
-	        if (val == 0) {
-	            pc = elsetarget;
-	        } else {
-			   ++pc;
+		if (intExpr) {
+			int val = intExpr->eval();
+			if (val == 0) {
+				pc = elsetarget;
+			} else {
+				++pc;
 			}
-    	} else if (strExpr) {
-        	string val = strExpr->eval();
-            if (val == "") {		//this is supposed to increment the program counter when true -> done
-                ++pc;
-            } else {	//this is supposed to assign the pc to elsetarget -> done
-            	pc = elsetarget;
-            }
-        } else if (intIdExpr) {
-            int val = intIdExpr->eval();
-            if (val == 0) {
-              pc = elsetarget;
-            } else {
-              ++pc;
-          }
-        } else if (strIdExpr) {
-          string val = strIdExpr->eval();
-          if (val == "") {
-            ++pc;
-          } else {
-            pc = elsetarget;
-          }
-        } else if (strPostFixExpr) {
-        	string val = strPostFixExpr->eval();
-        	if (val == "") {
-        		pc = elsetarget;
-        	} else {
-        		++pc;
-        	}
-        } else if (intPostFixExpr) {
-        	int val = intPostFixExpr->eval();
-        	if (val == 0) {
-        		pc = elsetarget;
-        	} else {
-        		++pc;
-        	}
-	}
-};
-
-class GoToStmt : public Stmt{
-  //Emma
-private:
-	int target;
-public:
-	GoToStmt(){
-       target = 0;
-     }
-	~GoToStmt(){}
-	void setTarget(int t){
-       target = t;
-    }
-	string toString(){
-       return "GoTo " + to_string(target);
-    }
-	void execute(){
-      pc = target;
-    }
-
-};
-
-class Compiler{
-private:
-	void buildIf(){
-       ++tokitr;
-       ++lexitr;
-       Expr* condition = nullptr;
-       buildExpr();
-       if(*tokitr == "t_jump"){
-         cerr << "Incorrect Syntax" << endl;
-         exit(-1);
-       }
-       ++tokitr;
-       int line = stoi(*tokitr);
-       ++lexitr;
-       IfStmt* ifstmt = new IfStmt();
-       ifstmt -> setExpr(condition);
-       ifstmt -> setTarget(line);
-       insttable.push_back(ifstmt);
-
-    }
-
-	void buildWhile() {	//need two increments, only goes over t-while currently
-		++tokitr;
-		++lexitr;
-		Expr* condition = buildExpr();
-		WhileStmt* whileStmt = new WhileStmt(condition, 0);
-		insttable.push_back(whileStmt);
-		if (*tokitr != "{") {
-			cerr << "Error: Expected '{' to start while loop body" << endl;
-			return;
-		}
-		++tokitr;
-		++lexitr;
-		while (*tokitr != "}") {
-			buildStmt();
-		}
-		if (*tokitr != "}") {
-			cerr << "Error: Expected '}' to close while loop body" << endl;
-			return;
-		}
-		int jumpTarget = insttable.size();
-		whileStmt->setTarget(jumpTarget);
-		++tokitr;
-		++lexitr;
-	}
-
-	void buildStmt(){
-		//Patrick
-		while (*tokitr != "s_rbrace") {
-			if (*tokitr == "t_if") {
-				buildIf();
-			} else if (*tokitr == "t_while") {
-				buildWhile();
-			} else if (*tokitr == "t_id") {
-				buildAssign();
-			} else if (*tokitr == "t_input") {
-				buildInput();
-			} else if (*tokitr == "t_output") {
-				buildOutput();
+		} else if (strExpr) {
+			string val = strExpr->eval();
+			if (val == "") {		//this is supposed to increment the program counter when true -> done
+				++pc;
+			} else {	//this is supposed to assign the pc to elsetarget -> done
+				pc = elsetarget;
+			}
+		} else if (intIdExpr) {
+			int val = intIdExpr->eval();
+			if (val == 0) {
+				pc = elsetarget;
+			} else {
+				++pc;
+			}
+		} else if (strIdExpr) {
+			string val = strIdExpr->eval();
+			if (val == "") {
+				++pc;
+			} else {
+				pc = elsetarget;
+			}
+		} else if (strPostFixExpr) {
+			string* val = strPostFixExpr->eval();
+			if (*val == "") {
+				pc = elsetarget;
+			} else {
+				++pc;
+			}
+		} else if (intPostFixExpr) {
+			int val = intPostFixExpr->eval();
+			if (val == 0) {
+				pc = elsetarget;
+			} else {
+				++pc;
 			}
 		}
-	}
-	void buildAssign(){
-		//Patrick
-		string id = *lexitr;
-		tokitr++; lexitr++;	//itterates over t_id
-        tokitr++; lexitr++;	//itterate over s_assign
-        Expr* expr = buildExpr();
-        tokitr++; lexitr++;	//itterate over s_semi
-        AssignStmt* assign = new AssignStmt(id, expr);
-        insttable.push_back(assign);
-	}
-	void buildInput(){
-      ++tokitr;
-      string var = *lexitr;
-      ++lexitr;
-      InputStmt* input = new InputStmt();
-      input->setVar(var);
-      insttable.push_back(input);
-    }
-	void buildOutput(){
-		//Patrick
-		tokitr++; lexitr++; //iterate over s_lparen
-        if(*tokitr == "t_text"){
-          	string text = *lexitr;
-            tokitr++; lexitr++; //iterate over t_text
-        	StrOutStmt* strout = new StrOutStmt(text);
-            insttable.push_back(strout);
-        }
-        else{
-         	Expr* expr = buildExpr();
-         	ExprOutStmt* strout = new ExprOutStmt(expr);
-            insttable.push_back(strout);
-        }
-        tokitr++; lexitr++; //iterate over s_rparen
-	}
+		}
+	};
 
-	Expr* buildExpr() {
-		vector<string> outputQueue;
-		vector<string> opStack;
-		bool isIntExpr = true;
-		bool hasInt = false;
-		bool hasStr = false;
+	class GoToStmt : public Stmt{
+		//Emma
+	private:
+		int target;
+	public:
+		GoToStmt(){
+			target = 0;
+		}
+		~GoToStmt(){}
+		void setTarget(int t){
+			target = t;
+		}
+		string toString(){
+			return "GoTo " + to_string(target);
+		}
+		void execute(){
+			pc = target;
+		}
 
-		while (*tokitr != "s_semi" && *tokitr != "s_rparen") {
-			string token = *tokitr;
-			string lexeme = *lexitr;
+	};
 
-			if (token == "t_number") {
-				hasInt = true;
-				outputQueue.push_back(lexeme);
+	class Compiler{
+	private:
+		void buildIf(){
+			++tokitr;
+			++lexitr;
+			Expr* condition = nullptr;
+			buildExpr();
+			if(*tokitr == "t_jump"){
+				cerr << "Incorrect Syntax" << endl;
+				exit(-1);
 			}
-			else if (token == "t_text") {
-				hasStr = true;
-				outputQueue.push_back(lexeme);
-			}
-			else if (token == "t_id") {
-				if (symboltable.find(lexeme) != symboltable.end()) {
-					if (symboltable[lexeme] == "t_string") {
-						hasStr = true;
-					} else {
-						hasInt = true;
-					}
-				}
-				outputQueue.push_back(lexeme);
-			}
-			else if (precMap.find(token) != precMap.end()) {
-				// Operator
-				while (!opStack.empty() && precMap[opStack.back()] >= precMap[lexeme]) {
-					outputQueue.push_back(opStack.back());
-					opStack.pop_back();
-				}
-				opStack.push_back(lexeme);
-			}
-			else if (token == "s_lparen") {
-				opStack.push_back(lexeme);
-			}
-			else if (token == "s_rparen") {
-				while (!opStack.empty() && opStack.back() != "(") {
-					outputQueue.push_back(opStack.back());
-					opStack.pop_back();
-				}
-				if (!opStack.empty() && opStack.back() == "(") {
-					opStack.pop_back();
-				}
-			}
+			++tokitr;
+			int line = stoi(*tokitr);
+			++lexitr;
+			IfStmt* ifstmt = new IfStmt();
+			ifstmt -> setExpr(condition);
+			ifstmt -> setTarget(line);
+			insttable.push_back(ifstmt);
 
+		}
+
+		void buildWhile() {	//need two increments, only goes over t-while currently
+			++tokitr;
+			++lexitr;
+			Expr* condition = buildExpr();
+			WhileStmt* whileStmt = new WhileStmt(condition, 0);
+			insttable.push_back(whileStmt);
+			if (*tokitr != "{") {
+				cerr << "Error: Expected '{' to start while loop body" << endl;
+				return;
+			}
+			++tokitr;
+			++lexitr;
+			while (*tokitr != "}") {
+				buildStmt();
+			}
+			if (*tokitr != "}") {
+				cerr << "Error: Expected '}' to close while loop body" << endl;
+				return;
+			}
+			int jumpTarget = insttable.size();
+			whileStmt->setTarget(jumpTarget);
 			++tokitr;
 			++lexitr;
 		}
 
-		while (!opStack.empty()) {
-			outputQueue.push_back(opStack.back());
-			opStack.pop_back();
+		void buildStmt(){
+			//Patrick
+			while (*tokitr != "s_rbrace") {
+				if (*tokitr == "t_if") {
+					buildIf();
+				} else if (*tokitr == "t_while") {
+					buildWhile();
+				} else if (*tokitr == "t_id") {
+					buildAssign();
+				} else if (*tokitr == "t_input") {
+					buildInput();
+				} else if (*tokitr == "t_output") {
+					buildOutput();
+				}
+			}
+		}
+		void buildAssign(){
+			//Patrick
+			string id = *lexitr;
+			tokitr++; lexitr++;	//itterates over t_id
+			tokitr++; lexitr++;	//itterate over s_assign
+			Expr* expr = buildExpr();
+			tokitr++; lexitr++;	//itterate over s_semi
+			AssignStmt* assign = new AssignStmt(id, expr);
+			insttable.push_back(assign);
+		}
+		void buildInput(){
+			++tokitr;
+			string var = *lexitr;
+			++lexitr;
+			InputStmt* input = new InputStmt();
+			input->setVar(var);
+			insttable.push_back(input);
+		}
+		void buildOutput(){
+			//Patrick
+			tokitr++; lexitr++; //iterate over s_lparen
+			if(*tokitr == "t_text"){
+				string text = *lexitr;
+				tokitr++; lexitr++; //iterate over t_text
+				StrOutStmt* strout = new StrOutStmt(text);
+				insttable.push_back(strout);
+			}
+			else{
+				Expr* expr = buildExpr();
+				ExprOutStmt* strout = new ExprOutStmt(expr);
+				insttable.push_back(strout);
+			}
+			tokitr++; lexitr++; //iterate over s_rparen
 		}
 
-		if (hasInt && hasStr) {
-			cerr << "Type error: cannot mix integers and strings in an expression." << endl;
+		Expr* buildExpr() {
+			vector<string> outputQueue;
+			vector<string> opStack;
+			bool isIntExpr = true;
+			bool hasInt = false;
+			bool hasStr = false;
+
+			while (*tokitr != "s_semi" && *tokitr != "s_rparen") {
+				string token = *tokitr;
+				string lexeme = *lexitr;
+
+				if (token == "t_number") {
+					hasInt = true;
+					outputQueue.push_back(lexeme);
+				}
+				else if (token == "t_text") {
+					hasStr = true;
+					outputQueue.push_back(lexeme);
+				}
+				else if (token == "t_id") {
+					if (symboltable.find(lexeme) != symboltable.end()) {
+						if (symboltable[lexeme] == "t_string") {
+							hasStr = true;
+						} else {
+							hasInt = true;
+						}
+					}
+					outputQueue.push_back(lexeme);
+				}
+				else if (precMap.find(token) != precMap.end()) {
+					// Operator
+					while (!opStack.empty() && precMap[opStack.back()] >= precMap[lexeme]) {
+						outputQueue.push_back(opStack.back());
+						opStack.pop_back();
+					}
+					opStack.push_back(lexeme);
+				}
+				else if (token == "s_lparen") {
+					opStack.push_back(lexeme);
+				}
+				else if (token == "s_rparen") {
+					while (!opStack.empty() && opStack.back() != "(") {
+						outputQueue.push_back(opStack.back());
+						opStack.pop_back();
+					}
+					if (!opStack.empty() && opStack.back() == "(") {
+						opStack.pop_back();
+					}
+				}
+
+				++tokitr;
+				++lexitr;
+			}
+
+			while (!opStack.empty()) {
+				outputQueue.push_back(opStack.back());
+				opStack.pop_back();
+			}
+
+			if (hasInt && hasStr) {
+				cerr << "Type error: cannot mix integers and strings in an expression." << endl;
+			}
+
+			if (hasStr) {
+				return new StrPostFixExpr(outputQueue);
+			} else {
+				return new IntPostFixExpr(outputQueue);
+			}
 		}
 
-		if (hasStr) {
-			return new StrPostFixExpr(outputQueue);
-		} else {
-			return new IntPostFixExpr(outputQueue);
+
+		// headers for populate methods may not change
+		void populateTokenLexemes(istream& infile){
+			string token, lexeme;
+			while (infile >> token >> lexeme) {
+				tokens.push_back(token);
+				lexemes.push_back(lexeme);
+			}
+
+			tokitr = tokens.begin();
+			lexitr = lexemes.begin();
+		};
+		void populateSymbolTable(istream& infile){
+			string id, datatype;
+			while (infile >> id >> datatype >> datatype) {
+				symboltable[id] = datatype;
+			}
 		}
+
+	public:
+		Compiler(){}
+		// headers may not change
+		Compiler(istream& source, istream& symbols){
+			// build precMap - include logical, relational, arithmetic operators
+			precMap["or"] = 5;
+			precMap["and"] = 4;
+			precMap["<"] = 3;
+			precMap[">"] = 3;
+			precMap["<="] = 3;
+			precMap[">="] = 3;
+			precMap["!="] = 3;
+			precMap["=="] = 3;
+			precMap["+"] = 2;
+			precMap["-"] = 2;
+			precMap["*"] = 1;
+			precMap["/"] = 1;
+			precMap["%"] = 1;
+
+			populateTokenLexemes(source);	//Copy over lexemes data file readin code
+			populateSymbolTable(symbols);	//Reading in the symbol table output file in variable name, data type : lexeme, token
+		}
+
+		// The compile method is responsible for getting the instruction
+		// table built.  It will call the appropriate build methods.
+		bool compile(){
+			tokitr++; lexitr++; //iterate over t_main
+			tokitr++; lexitr++; //iterate over s_lbrace
+			buildStmt();
+			tokitr++; lexitr++; //iterate over s_rbrace
+			if(tokitr != tokens.end()){
+				cerr<< "Error: " << *tokitr <<  " " << *lexitr << " is invalid"<<endl;
+				return false;
+			}
+			cout<< "Compile Output Success!"<<endl;
+			return true;
+		}
+
+		// The run method will execute the code in the instruction
+		// table.
+		void run(){
+			pc = 0;
+			while (pc < insttable.size()) {
+				insttable[pc]->execute();
+			}
+		};
+	};
+
+	// prints vartable, instable, symboltable
+	void dump();
+	int main(){
+
+		ifstream source("sourcelexemes.txt");
+		ifstream symbols("vars.txt");
+		if (!source || !symbols) exit(-1);
+		Compiler c(source, symbols);
+		c.compile();
+		// might want to call dump to check if everything built correctly
+		// dump();
+		c.run();
+
+		return 0;
 	}
-
-
-	// headers for populate methods may not change
-	void populateTokenLexemes(istream& infile){
-		string token, lexeme;
-		while (infile >> token >> lexeme) {
-			tokens.push_back(token);
-			lexemes.push_back(lexeme);
-		}
-
-		tokitr = tokens.begin();
-		lexitr = lexemes.begin();
-    };
-	void populateSymbolTable(istream& infile){
-          string id, datatype;
-          while (infile >> id >> datatype >> datatype) {
-            symboltable[id] = datatype;
-          }
-     }
-
-public:
-	Compiler(){}
-	// headers may not change
-	Compiler(istream& source, istream& symbols){
-		// build precMap - include logical, relational, arithmetic operators
-		precMap["or"] = 5;
-		precMap["and"] = 4;
-		precMap["<"] = 3;
-		precMap[">"] = 3;
-		precMap["<="] = 3;
-		precMap[">="] = 3;
-		precMap["!="] = 3;
-		precMap["=="] = 3;
-		precMap["+"] = 2;
-		precMap["-"] = 2;
-		precMap["*"] = 1;
-		precMap["/"] = 1;
-		precMap["%"] = 1;
-
-		populateTokenLexemes(source);	//Copy over lexemes data file readin code
-		populateSymbolTable(symbols);	//Reading in the symbol table output file in variable name, data type : lexeme, token
-	}
-
-	// The compile method is responsible for getting the instruction
-	// table built.  It will call the appropriate build methods.
-	bool compile(){
-		tokitr++; lexitr++; //iterate over t_main
-        tokitr++; lexitr++; //iterate over s_lbrace
-        buildStmt();
-        tokitr++; lexitr++; //iterate over s_rbrace
-        if(tokitr != tokens.end()){
-            cerr<< "Error: " << *tokitr <<  " " << *lexitr << " is invalid"<<endl;
-            return false;
-        }
-          cout<< "Compile Output Success!"<<endl;
-          return true;
-	}
-
-	// The run method will execute the code in the instruction
-	// table.
-	void run(){
-		pc = 0;
-		while (pc < insttable.size()) {
-			insttable[pc]->execute();
-		}
-    };
-};
-
-// prints vartable, instable, symboltable
-void dump();
-int main(){
-
-	ifstream source("sourcelexemes.txt");
-	ifstream symbols("vars.txt");
-	if (!source || !symbols) exit(-1);
-	Compiler c(source, symbols);
-	c.compile();
-	// might want to call dump to check if everything built correctly
-	// dump();
-	c.run();
-
-	return 0;
-}
